7bd085ec43f7b9ab246dfb6251319e25b7e79271
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-02-26 17:28:24 +0200

    testing_jobs controller is for backend purposes only and can be removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :gt_metrix_tests
-  resources :testing_jobs, only: [:index]
+  resources :gt_metrix_tests, only: [:index, :show]
   resources :companies
   resource :company_import, :controller => 'company_import', only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
