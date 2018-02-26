758c47ea4c293bf018d7202d9b57dbfda7dbecb2
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-02-26 17:16:13 +0200

    Companies should be unique for "test" models

--- app/models/gt_metrix_test.rb
+++ app/models/gt_metrix_test.rb
@@ -1,3 +1,5 @@
 class GtMetrixTest < ApplicationRecord
+  validates :company_id, uniqueness: true
+
   belongs_to :company
 end
--- app/models/testing_job.rb
+++ app/models/testing_job.rb
@@ -1,3 +1,5 @@
 class TestingJob < ApplicationRecord
+  validates :company_id, uniqueness: true
+
   belongs_to :company
 end
