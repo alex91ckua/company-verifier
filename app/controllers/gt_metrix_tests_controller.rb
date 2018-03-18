418448d6ae9046637156d84f5fe96429aabc74c9
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-19 00:17:14 +0200

    API - gt metrix tests filtration added

--- app/controllers/gt_metrix_tests_controller.rb
+++ app/controllers/gt_metrix_tests_controller.rb
@@ -1,8 +1,14 @@
 class GtMetrixTestsController < ApplicationController
-  
+
+  has_scope :by_page_speed, using: [:from, :to], only: :index
+  has_scope :by_yslow_score, using: [:from, :to], only: :index
+
+  # Filters available
+  # ?by_page_speed[from]=79&by_page_speed[to]=999&by_yslow_score[from]=0&by_yslow_score[to]=80
+
   # GET /gt_metrix_tests
   def index
-    @gt_metrix_tests = GtMetrixTest.all
+    @gt_metrix_tests = apply_scopes(GtMetrixTest).all
 
     render json: @gt_metrix_tests
   end
--- app/models/gt_metrix_test.rb
+++ app/models/gt_metrix_test.rb
@@ -1,5 +1,8 @@
 class GtMetrixTest < ApplicationRecord
   validates :company_id, uniqueness: true
 
+  scope :by_page_speed, -> (from, to) { where("pagespeed_score >= ? AND pagespeed_score <= ?", from, to) }
+  scope :by_yslow_score, -> (from, to) { where("yslow_score >= ? AND yslow_score <= ?", from, to) }
+
   belongs_to :company
 end
